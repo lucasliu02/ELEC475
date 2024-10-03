3f71dc0c6cdec1d5e1e684af83ae65540fcc039f
Lucas Liu <lucasliu02@gmail.com>
2024-10-02 23:22:08 -0400

    working testing script & added test partition to training script for validation (loss plot)

---
+++ ELEC475_Lab2/test.py
@@ -0,0 +1,89 @@
+import argparse
+import os
+import csv
+import torch
+import numpy as np
+import matplotlib.pyplot as plt
+from torch.utils.data import DataLoader
+from torchvision.transforms import v2
+from math import dist
+from model import SnoutNetModel
+from data import SnoutNetDataset
+from statistics import mean, stdev
+
+def main():
+    argparser = argparse.ArgumentParser()
+    argparser.add_argument('-s', metavar='state', type=str, help='parameter file (.pth)')
+    argparser.add_argument('-r', metavar='results', help='results file (.csv)')
+    argparser.add_argument('-p', action='store_true', help='flag for plotting every result')
+
+    args = argparser.parse_args()
+
+    save_file = args.s if args.s is not None else 'weights.pth'
+    results_file = args.r if args.r is not None else 'results.csv'
+    plot_flag = args.p
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+    save_file = os.path.join('train_results', save_file)
+    results_file = os.path.join('test_results', results_file)
+
+    print('\t\tusing device ', device)
+
+    model = SnoutNetModel()
+    model.load_state_dict(torch.load(save_file))
+    model.to(device)
+    model.eval()
+
+    test_transform = v2.Compose([v2.ToImage(), v2.ToDtype(torch.float32, scale=True), v2.Resize([227, 227])])
+
+    label_path = os.path.join('data', 'oxford-iiit-pet-noses', 'test_noses.txt')
+    img_path = os.path.join('data', 'oxford-iiit-pet-noses', 'images-original', 'images')
+    test_set = SnoutNetDataset(label_path, img_path, test_transform)
+    test_loader = DataLoader(test_set, batch_size=1, shuffle=True)
+    it = iter(test_loader)
+
+    groundtruth_x = []
+    groundtruth_y = []
+    estimated_x = []
+    estimated_y = []
+    distances = []
+    minimum = 999
+    maximum = 0
+    for image, label in it:
+        image = image.to(device=device)
+        output = model(image)
+        gt_x, gt_y = label.tolist()[0]
+        e_x, e_y = output.tolist()[0]
+        distance = dist((gt_x, gt_y), (e_x, e_y))
+        groundtruth_x.append(gt_x)
+        groundtruth_y.append(gt_y)
+        estimated_x.append(e_x)
+        estimated_y.append(e_y)
+        distances.append(distance)
+        if distance < minimum:
+            minimum = distance
+        if distance > maximum:
+            maximum = distance
+        if plot_flag:
+            print('distance between estimated and ground truth pet nose locations: ', distance)
+            image = image.type(torch.FloatTensor).squeeze()
+            plt.imshow(image.permute(1, 2, 0), cmap='gray')
+            plt.plot(gt_x, gt_y, "ro", markersize=10)
+            plt.plot(e_x, e_y, "bo", markersize=10)
+            plt.show()
+
+    average = mean(distances)
+    std_dev = stdev(distances)
+    print('min:', minimum, '\nmean:', average, '\nmax:', maximum, '\nstd dev:', std_dev)
+    results = [groundtruth_x, groundtruth_y, estimated_x, estimated_y, distances]
+    results = np.array(results).T.tolist()
+
+
+    with open(results_file, 'w', newline='') as f:
+        writer = csv.writer(f)
+        writer.writerow(['groundtruth_x', 'groundtruth_y', 'estimated_x', 'estimated_y', 'distances'])
+        for item in results:
+            writer.writerow([item[0], item[1], item[2], item[3], item[4]])
+
+if __name__ == '__main__':
+    main()
--- ELEC475_Lab2/train.py
+++ ELEC475_Lab2/train.py
@@ -14,28 +14,41 @@ from model import SnoutNetModel
 from torch.utils.data import DataLoader
 
 
-def train(n_epochs, optimizer, model, loss_fn, train_loader, scheduler, device, save_file=None, plot_file=None):
-    model.train()
+def train(n_epochs, optimizer, model, loss_fn, train_loader, test_loader, scheduler, device, save_file=None, plot_file=None):
     losses_train = []
+    losses_test = []
 
     for epoch in range(1, n_epochs+1):
         print('epoch ', epoch)
         loss_train = 0
-        for images, labels in train_loader:
-            # images = images.view(images.shape[0], -1)
-            images = images.to(device=device)
-            labels = labels.to(device=device)
-            outputs = model(images)
-            loss = loss_fn(outputs, labels)
-            optimizer.zero_grad()
-            loss.backward()
-            optimizer.step()
-            loss_train += loss.item()
+        loss_test = 0
+
+        for phase, loader in [('train', iter(train_loader)), ('test', iter(test_loader))]:
+            print(phase)
+            for images, labels in loader:
+                images = images.to(device=device)
+                labels = labels.to(device=device)
+                if phase == 'test':
+                    model.eval()
+                    with torch.no_grad():
+                        outputs = model(images)
+                        loss = loss_fn(outputs, labels)
+                        loss_test += loss.item()
+                else:
+                    model.train()
+                    outputs = model(images)
+                    loss = loss_fn(outputs, labels)
+                    optimizer.zero_grad()
+                    loss.backward()
+                    optimizer.step()
+                    loss_train += loss.item()
+
         scheduler.step(loss_train)
         losses_train += [loss_train/len(train_loader)]
+        losses_test += [loss_test/len(test_loader)]
 
-        print('{} Epoch {}, Training loss {}'.format(
-            datetime.datetime.now(), epoch, loss_train/len(train_loader)))
+        print('{} Epoch {}, Training loss {}, Testing loss {}'.format(
+            datetime.datetime.now(), epoch, loss_train/len(train_loader), loss_test/len(test_loader)))
 
         if save_file is not None:
             torch.save(model.state_dict(), save_file)
@@ -43,11 +56,14 @@ def train(n_epochs, optimizer, model, loss_fn, train_loader, scheduler, device, 
             plt.figure(2, figsize=(12, 7))
             plt.clf()
             plt.plot(losses_train, label='train')
+            plt.plot(losses_test, label='test')
             plt.xlabel('epoch')
             plt.ylabel('loss')
             plt.legend(loc=1)
             print('saving ', plot_file)
             plt.savefig(plot_file)
+
+
 
 def init_weights(m):
     if type(m) == nn.Linear:
@@ -65,9 +81,14 @@ def main():
 
     save_file = args.s if args.s is not None else 'weights.pth'
     n_epochs = args.e if args.e is not None else 30
-    batch_size = args.b if args.b is not None else 256
+    batch_size = args.b if args.b is not None else 32
     plot_file = args.p if args.p is not None else 'plot.png'
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+    save_file = os.path.join('train_results', save_file)
+    plot_file = os.path.join('train_results', plot_file)
+
+    print('\t\tusing device ', device)
 
     # torch.set_default_device(device)
 
@@ -85,6 +106,10 @@ def main():
     optimizer = optim.Adam(model.parameters(), lr=1e-3, weight_decay=1e-5)
     scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimizer, 'min')
     loss_fn = nn.MSELoss(reduction='mean')
+
+    test_path = os.path.join('data', 'oxford-iiit-pet-noses', 'test_noses.txt')
+    test_set = SnoutNetDataset(test_path, img_path, train_transform)
+    test_loader = DataLoader(test_set, batch_size=batch_size, shuffle=False)
     
     train(
         n_epochs,
@@ -92,10 +117,11 @@ def main():
         model,
         loss_fn,
         train_loader,
+        test_loader,
         scheduler,
         device,
         save_file,
-        plot_file
+        plot_file,
     )
 
 if __name__ == '__main__':
